ed7cf32dddc76ffde4ec76337e00ca627c9cea25
Cory Anderson <cory.anderson6@yahoo.com>
2023-02-20 17:29:13 -0600

    updated JS file

--- weekend-salary.js
+++ weekend-salary.js
@@ -5,5 +5,18 @@ let employees = [];
 const table = document.querySelector('table');
 const monthlyCost= document.getElementById('monthlyCost');
 
+
 const addButton = document.getElementById('addButton');
 addButton.addEventListener('click',addEmployee);
+
+function addEmployee() {
+
+    const firstName = doucment.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const idNumber = document.getElementById('idNumber').value;
+    const jobTitle = document.getElementById('jobTitle').value;
+    const annualSalary = parseFloat(document.getElementById('annualSalary').value);
+}
+
+employees.push({ firstName, idNumber, jobTitle, annualSalary });
+
