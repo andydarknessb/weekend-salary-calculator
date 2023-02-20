7f330f2fbae97991477adc5f4ee8341548154167
Cory Anderson <cory.anderson6@yahoo.com>
2023-02-20 17:44:05 -0600

    save

--- weekend-salary.js
+++ weekend-salary.js
@@ -20,3 +20,24 @@ function addEmployee() {
 
 employees.push({ firstName, idNumber, jobTitle, annualSalary });
 
+const row = table.insertRow();
+const cellFirstName = row.insertCell(0);
+const cellIdNumber = row.insertCell(1);
+const cellJobTitle = row.inserctCell(2);
+const cellAnnualSalary = row.insertCell(3);
+const cellDeleteButton = row.inserctCell(4);
+cellFirstName.innerHTML = firstName;
+cellIdNumber.innerHTML = idNumber;
+cellJobTitle.innerHTML = jobTitle;
+cellAnnualSalary.innerHTML = annualSalary.tofixed(2);
+cellDeleteButton.innerHTML = annualSalary.tofixed(2);
+const deleteButton = cellDeleteButton.querySelector('.deleteButton');
+deleteButton.addEventListener('click', function () {
+
+    const index = employees.findIndex((employee) => employee.idNumber === idNumber);
+    employees.splice(index, 1);
+
+    table.deleteRow(row.rowIndex);
+
+    updateMonthlyCost();
+});
