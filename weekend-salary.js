6b3e141fe2ba156a6120923dda19c3b953097a3c
Cory Anderson <cory.anderson6@yahoo.com>
2023-02-20 17:00:39 -0600

    updated html

--- weekend-salary.js
+++ weekend-salary.js
@@ -4,3 +4,6 @@ let employees = [];
 
 const table = document.querySelector('table');
 const monthlyCost= document.getElementById('monthlyCost');
+
+const addButton = document.getElementById('addButton');
+addButton.addEventListener('click',addEmployee);
