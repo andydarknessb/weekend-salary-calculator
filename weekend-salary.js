5c247056df6a950497c0fe8d946c5daeb56d13e0
Cory Anderson <cory.anderson6@yahoo.com>
2023-02-21 00:08:16 -0600

    updating js file

--- weekend-salary.js
+++ weekend-salary.js
@@ -1,14 +1,13 @@
 console.log ("Hello World");
 
-let employees = []
+let employees = [];
+let totalRow;
 
 const table = document.querySelector('table');
 const monthlyCost = document.getElementById('monthlyCost');
 
-
 const addButton = document.getElementById('addButton');
 addButton.addEventListener('click', addEmployee);
-
 
 function addEmployee() {
     const firstName = document.getElementById('firstName').value;
@@ -45,35 +44,46 @@ function addEmployee() {
     document.getElementById('jobTitle').value = '';
     document.getElementById('annualSalary').value = '';
 
-    cellDeleteButton.innerHTML = '<button class="deleteButton">Delete</button>';
+    cellDeleteButton.innerHTML = '<button class="deleteButton" data-salary="' + annualSalary + '">Delete</button>';
     const deleteButton = cellDeleteButton.querySelector('.deleteButton');
     deleteButton.addEventListener('click', function () {
         const index = employees.findIndex((employee) => employee.idNumber === idNumber);
         employees.splice(index, 1);
         table.deleteRow(row.rowIndex);
-        updateMonthlyCost();
+        const salary = parseFloat(deleteButton.getAttribute('data-salary'));
+        const monthlySalary = -(salary / 12);
+        updateMonthlyCost(monthlySalary);
     });
+    
 
     updateMonthlyCost();
+    // show all placeholders
+    document.getElementById('firstName').value = '';
+    document.getElementById('firstName').placeholder = 'First Name';
+    document.getElementById('lastName').value = '';
+    document.getElementById('lastName').placeholder = 'Last Name';
+    document.getElementById('idNumber').value = '';
+    document.getElementById('idNumber').placeholder = 'ID Number';
+    document.getElementById('jobTitle').value = '';
+    document.getElementById('jobTitle').placeholder = 'Job Title';
+    document.getElementById('annualSalary').value = '';
+    document.getElementById('annualSalary').placeholder = 'Annual Salary';
+
 }
 
 
 function updateMonthlyCost(salaryToRemove = 0) {
     const totalSalary = employees.reduce(function(acc, cur) {
-      return acc + cur.annualSalary;
+        return acc + cur.annualSalary;
     }, 0) - salaryToRemove;
     const monthlySalary = (totalSalary / 12).toLocaleString('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits: 2});
     monthlyCost.innerHTML = monthlySalary;
-  
-    if (totalSalary > 240000) {
-      monthlyCost.style.backgroundColor = 'red';
-    } else {
-      monthlyCost.style.backgroundColor = 'white';
 
-    const row = table.insertRow();
-    const cellTotalLabel = row.insertCell(0);
-    cellTotalLabel.innerHTML = 'Total Monthly Cost:';
-    const cellTotal = row.insertCell(1);
-    cellTotal.id = 'total';
+    if (totalSalary > 240000) {
+        monthlyCost.style.backgroundColor = 'red';
+    } else {
+        monthlyCost.style.backgroundColor = 'white';
+    }
 }
-  }
+
+
