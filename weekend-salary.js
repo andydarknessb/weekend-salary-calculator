35ca800e4fc2dc52f9f56fde3419c29852b4d96f
Cory Anderson <cory.anderson6@yahoo.com>
2023-02-20 20:52:05 -0600

    updated JS

--- weekend-salary.js
+++ weekend-salary.js
@@ -1,59 +1,47 @@
 console.log ("Hello World");
 
 
-let employees = [];
-
-
-const table = document.querySelector('table');
-const monthlyCost = document.getElementById('monthlyCost');
-
-
-const addButton = document.getElementById('addButton');
-addButton.addEventListener('click', addEmployee);
-
 function addEmployee() {
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const idNumber = document.getElementById('idNumber').value;
+    const jobTitle = document.getElementById('jobTitle').value;
+    const annualSalary = parseFloat(document.getElementById('annualSalary').value);
   
-  const firstName = document.getElementById('firstName').value;
-  const lastName = document.getElementById('lastName').value;
-  const idNumber = document.getElementById('idNumber').value;
-  const jobTitle = document.getElementById('jobTitle').value;
-  const annualSalary = parseFloat(document.getElementById('annualSalary').value);
-
-
-  employees.push({ firstName, lastName, idNumber, jobTitle, annualSalary });
-
-  const row = table.insertRow();
-  const cellFirstName = row.insertCell(0);
-  const cellLastName = row.insertCell (1);
-  const cellIdNumber = row.insertCell(2);
-  const cellJobTitle = row.insertCell(3);
-  const cellAnnualSalary = row.insertCell(4);
-  const cellDeleteButton = row.insertCell(5);
-  cellFirstName.innerHTML = firstName;
-  cellLastName.innerHTML = lastName;
-  cellIdNumber.innerHTML = idNumber;
-  cellJobTitle.innerHTML = jobTitle;
-
-  const annualSalaryFormatted = annualSalary.toLocaleString('en-US');
-  cellAnnualSalary.innerHTML = '$' + annualSalaryFormatted;
-
+    employees.push({ firstName, lastName, idNumber, jobTitle, annualSalary });
   
-
-  cellDeleteButton.innerHTML = '<button class="deleteButton">Delete</button>';
-  const deleteButton = cellDeleteButton.querySelector('.deleteButton');
-  deleteButton.addEventListener('click', function () {
-   
-    const index = employees.findIndex((employee) => employee.idNumber === idNumber);
-    employees.splice(index, 1);
-   
-    table.deleteRow(row.rowIndex);
-   
+    const row = table.insertRow();
+    const cellFirstName = row.insertCell(0);
+    const cellLastName = row.insertCell(1);
+    const cellIdNumber = row.insertCell(2);
+    const cellJobTitle = row.insertCell(3);
+    const cellAnnualSalary = row.insertCell(4);
+    const cellDeleteButton = row.insertCell(5);
+    cellFirstName.innerHTML = firstName;
+    cellLastName.innerHTML = lastName;
+    cellIdNumber.innerHTML = idNumber;
+    cellJobTitle.innerHTML = jobTitle;
+  
+    const annualSalaryFormatted = annualSalary.toLocaleString('en-US');
+    cellAnnualSalary.innerHTML = '$' + annualSalaryFormatted;
+  
+    document.getElementById('firstName').value = '';
+    document.getElementById('lastName').value = '';
+    document.getElementById('idNumber').value = '';
+    document.getElementById('jobTitle').value = '';
+    document.getElementById('annualSalary').value = '';
+  
+    cellDeleteButton.innerHTML = '<button class="deleteButton">Delete</button>';
+    const deleteButton = cellDeleteButton.querySelector('.deleteButton');
+    deleteButton.addEventListener('click', function () {
+      const index = employees.findIndex((employee) => employee.idNumber === idNumber);
+      employees.splice(index, 1);
+      table.deleteRow(row.rowIndex);
+      updateMonthlyCost();
+    });
+  
     updateMonthlyCost();
-  });
-
- 
-  updateMonthlyCost();
-}
+  }
 
 function updateMonthlyCost(salaryToRemove = 0) {
     const totalSalary = employees.reduce(function(acc, cur) {
